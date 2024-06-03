0adf2ebbd925f0819fd72bd5ac4c7f50bf2c3d3b
DiasEI <fjfd2000@sapo.pt>
2024-06-03 18:28:35 +0100

    Updated Auth.js

--- controllers/v2/auth.js
+++ controllers/v2/auth.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs/dist/bcrypt');
 const authenticateUtil = require('../../utils/authenticate.js');
 
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 exports.signin = async (req, res) => {
     try {
@@ -12,25 +12,25 @@ exports.signin = async (req, res) => {
             where: {
                 username: username,
             },
-        })
+        });
 
-        if (user) {
-            var passwordIsValid = bcrypt.compareSync(
-                password,
-                user.password
-            );
-
-            if (passwordIsValid) {
-                const accessToken = authenticateUtil.generateAccessToken({ id: user.id, nome: user.nome, email: user.email });
-                res.status(200).json({ nome: user.nome, token: accessToken });
-            }
+        if (!user) {
+            return res.status(404).json({ code: 404, msg: 'User not found' });
         }
 
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        if (!passwordIsValid) {
+            return res.status(401).json({ code: 401, msg: 'Invalid password' });
+        }
+
+        const accessToken = authenticateUtil.generateAccessToken({ id: user.id, nome: user.nome, email: user.email });
+        res.status(200).json({ code: 200, nome: user.nome, token: accessToken });
+
     } catch (error) {
-        res.status(401).json({ msg: error.message })
+        res.status(500).json({ code: 500, msg: error.message });
     }
 }
-
 
 exports.signup = async (req, res) => {
     try {
@@ -41,22 +41,24 @@ exports.signup = async (req, res) => {
         });
 
         if (existingUser) {
-            return res.status(400).json({ msg: 'Username already taken' });
+            return res.status(400).json({ code: 400, msg: 'Username already taken' });
         }
 
-        await prisma.users.create({
+        const newUser = await prisma.Users.create({
             data: {
                 username: username,
                 email: email,
                 nome: nome,
                 telemovel: telemovel,
                 password: bcrypt.hashSync(password, 8),
-
             },
-        })
+        });
 
-        return this.signin(req, res);
+        // Automatically sign in the user after successful registration
+        const accessToken = authenticateUtil.generateAccessToken({ id: newUser.id, nome: newUser.nome, email: newUser.email });
+        return res.status(200).json({ code: 200, nome: newUser.nome, token: accessToken });
+
     } catch (error) {
-        res.status(401).json({ msg: error.message })
+        res.status(500).json({ code: 500, msg: error.message });
     }
 }
