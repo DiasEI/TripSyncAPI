5eec266db747692f1897c7b8e7bd0746e75a80cf
aluisaoliveira <aluisaoliveira@ipvc.pt>
2024-06-11 00:50:56 +0100

    viagem

--- routes/v2/viagens.js
+++ routes/v2/viagens.js
@@ -7,5 +7,5 @@ viagemRouter.get('/:idr', controller.getById); //read one by his id
 viagemRouter.post('/create', controller.create); //create new viagem
 viagemRouter.put('/update', controller.update); //update viagem
 viagemRouter.delete('/delete/:idr', controller.delete); //delete viagem
-
+viagemRouter.get('/byUser/:idr', controller.getViagensByUser);
 module.exports = viagemRouter;
