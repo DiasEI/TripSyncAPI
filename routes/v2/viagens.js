5e9495d428671c457d096bf8a39afcc8bae04d36
domems <domems@gmail.com>
2024-06-16 01:33:58 +0100

    Lista Viagens

--- routes/v2/viagens.js
+++ routes/v2/viagens.js
@@ -3,9 +3,9 @@ const controller = require('../../controllers/v2/viagem');
 
 //viagens CRUD
 viagemRouter.get('/', controller.getAll); //read all
-viagemRouter.get('/:idr', controller.getById); //read one by his id
+viagemRouter.get('/:id', controller.getById); //read one by his id
 viagemRouter.post('/create', controller.create); //create new viagem
 viagemRouter.put('/update', controller.update); //update viagem
-viagemRouter.delete('/delete/:idr', controller.delete); //delete viagem
-viagemRouter.get('/byUser/:idr', controller.getViagensByUser);
+viagemRouter.delete('/delete/:id', controller.delete); //delete viagem
+viagemRouter.get('/byUser/:id', controller.getViagensByUser);
 module.exports = viagemRouter;
